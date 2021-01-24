5f3ae67c9ca65d6f2e54c68bf0e9a2cd8af276e5
sylviaxliu <sylviaxliu@yahoo.com>
2021-01-24 00:27:30 -0500

    move graphics

--- venmoprog.py
+++ venmoprog.py
@@ -18,18 +18,21 @@ def main():
     username = tk.StringVar(value='entry_font')
     password = tk.StringVar(value='entry_font')
 
+    welcomelabel = tk.Label(loginwindow, text="hello and welcome to money buddy!")
+    welcomelabel.grid(column = 1, row = 1)
+
     userlabel = tk.Label(loginwindow, text="Username:  ",width = 14)
     userlabel.config(font=("Consolas",16))
-    userlabel.grid(column=1, row=1)
+    userlabel.grid(column=1, row=2)
     userinput = tk.Entry(loginwindow, width = 20, textvariable = username, font=("Consolas",12))
-    userinput.grid(column=1, row=2)
+    userinput.grid(column=2, row=2)
 
     passlabel = tk.Label(loginwindow, text = "Password:  ", width = 14)
     passlabel.config(font=("Consolas", 16))
     passlabel.grid(column=1, row = 3)
     
     passinput = tk.Entry(loginwindow, width = 20, textvariable = password, font=("Consolas",12), show="*")
-    passinput.grid(column=1, row = 4)
+    passinput.grid(column=2, row = 3)
     
     
 
