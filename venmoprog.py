17ca72c3b781950101a3be9528de9ca11926b798
pzjiang <pzjiang01@hotmail.com>
2021-01-23 22:17:11 -0500

    first

---
+++ venmoprog.py
@@ -0,0 +1,98 @@
+from venmo_api import Client
+
+
+access_token = Client.get_access_token(username="", password="");
+
+venmo = Client(access_token=access_token)
+
+
+users = venmo.user.search_for_users(query="Peter");
+for user in users:
+    print(user.username)
+
+"""
+#in order to log out:
+venmo.log_out("Bearer access_token")
+#access_token is a variable that holds the value of the access code. replace with the given access token
+
+"""
+
+
+"""
+#structure of transaction
+
+
+
+
+
+"""
+
+
+def callback(transactions_list):
+    for transaction in transactions_list:
+        print(transaction)
+        #print(transaction.id)
+        #print(type(transaction))
+        #break;
+
+used = venmo.user.get_my_profile();
+
+"""
+print("type below");
+print(type(used));
+print("==-==-=-=-=-=");
+"""
+
+venmo.user.get_user_transactions(user=used,callback=callback,limit=1000)
+
+
+    
+#venmo.log_out("Bearer " + access_token)
+#print(access_token)
+
+
+"""
+try:
+    #this is to search for users
+    users = venmo.user.search_for_users(query="Peter");
+    for user in users:
+        print(user.username)
+
+
+    #use callback to make it multi-threaded
+
+    def callback(users):
+        for user in users:
+            print(user.username)
+    venmo.user.search_for_users(query="",callback=callback,page=2,count=10)
+
+
+
+
+    
+    #need to log out manually access token doesn't expire
+    venmo.log_out("")
+
+    #request money
+    venmo.payment.request_money(money int, "message", id)
+
+
+    #send money
+    venmo.payment.send_money(money int, "messsage", id)
+
+
+
+    #callback to get transactions
+    def callback(transactions_list):
+        for transaction in transactions_list:
+            print(transaction)
+
+    venmo_api.user.get_user_transactions(user_id="", callback=callback)
+
+
+
+except:
+    print("an error has occured");
+"""
+
+
