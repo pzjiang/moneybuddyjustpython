a7adc497185c3e653ffc7cf51b2f775eb6ff42d1
pzjiang <pzjiang01@hotmail.com>
2021-01-24 02:15:45 -0500

    ok

--- venmoprog.py
+++ venmoprog.py
@@ -11,14 +11,44 @@ from graphics import *
 def submit(username1, password1, access_token):
     venmo1 = None
 
+    
+
    
     try:
-        Client.get_access_token(username=username1, password=password1)
+        venmo1 = Client.get_access_token(username=username1, password=password1)
     except:
         messagebox.showerror(title="Login Error", message="Invalid credentials, please try again :(")
         return "F"
 
+    total = 0
+    relationdict = {}
+
+    used = venmo1.user.get_my_profile();
+    valueMul = 1
+
+    
+    def callback(transactions_list):
+        for transaction in transactions_list:
+            valueMul = 1
+            if transaction.payment_type != "pay":
+                valueMul = -1
+            if transaction.actor.username == used.username:
+                total -= transaction.amount * valueMul
+                relationdict[transaction.target.username] -= transaction.amount * valueMul
+            else:
+                 print ("no")
+
+    """
+    print("type below");
+    print(type(used));
+    print("==-==-=-=-=-=");
+    """
+
+    venmo1.user.get_user_transactions(user=used,callback=callback,limit=1000)
+
     window = GraphWin("Analysis",1200,800)
+
+    
     
     
 
@@ -93,21 +123,26 @@ def main():
 
 
 
-main()
+#main()
 
 
-
+#f8768ce8edbd17afe00ec7f576c0da42867a1a8dc44de3184c6a36f68bd8747f
 def test():
         
-    access_token = Client.get_access_token(username="", password="");
-
+    access_token = Client.get_access_token(username="Peter-Jiang-8", password="Tigers1614!");
+    #access_token = "4fa83ce2e92005ff3c3c5d3398d256d356897686ded129c8340af93a55dffd29"
+    
     venmo = Client(access_token=access_token)
 
+    #venmo.log_out("Bearer 4fa83ce2e92005ff3c3c5d3398d256d356897686ded129c8340af93a55dffd29")
 
+    """
     users = venmo.user.search_for_users(query="Peter");
     for user in users:
         print(user.username)
 
+
+    """
     """
     #in order to log out:
     venmo.log_out("Bearer access_token")
@@ -119,7 +154,7 @@ def test():
     """
     #structure of transaction
 
-
+Transaction: (id=2901185891756146731, payment_id=2901185891152167144, date_completed=1576634309, date_created=1576634308, date_updated=1581921114, payment_type=pay, amount=31.0, audience=friends, status=settled, note=To fund your pro gaming career, device_used=iPhone, actor=User: (id=2895490236547072484, username=Peter-Jiang-8, first_name=Peter, last_name=Jiang, display_name=Peter Jiang, phone=None, profile_picture_url=https://s3.amazonaws.com/venmo/no-image.gif, about= , date_joined=1575955334, is_group=False, is_active=True), target=User: (id=2673829792972800777, username=Rachel-Zhang-34, first_name=Rachel, last_name=Zhang, display_name=Rachel Zhang, phone=None, profile_picture_url=https://pics.venmo.com/02fb030c-3c86-48fc-8cb0-a4f32e582cce?width=460&height=460&photoVersion=2, about= , date_joined=1549531350, is_group=False, is_active=True))
 
 
 
@@ -128,8 +163,9 @@ def test():
 
     def callback(transactions_list):
         for transaction in transactions_list:
-            print(transaction)
-            #print(transaction.id)
+            #print(transaction)
+            print(transaction.payment_type=="pay")
+            print(transaction.target.first_name)
             #print(type(transaction))
             #break;
 
@@ -143,7 +179,7 @@ def test():
 
     venmo.user.get_user_transactions(user=used,callback=callback,limit=1000)
 
-
+    venmo.log_out("Bearer " + access_token)
         
     #venmo.log_out("Bearer " + access_token)
     #print(access_token)
